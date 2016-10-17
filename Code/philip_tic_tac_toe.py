96bb5b23e0144f9c258fc9f1a52041d119d375cc
PhiliPdB <phlpdbrn@gmail.com>
2016-10-17 14:14:16 +0200

    Finish my tic tac toe

--- Code/philip_tic_tac_toe.py
+++ Code/philip_tic_tac_toe.py
@@ -1,7 +1,10 @@
-class Game:
-    board = [[0 for x in range(3)] for y in range(3)]
-    turn = 1
-    
+from copy import deepcopy
+
+class Game:   
+    def __init__(self, board=[[0 for x in range(3)] for y in range(3)], turn=1):
+        self.board = board
+        self.turn = turn
+
     def print_board(self):
         for row in self.board:
             print(row)
@@ -9,7 +12,6 @@ class Game:
     def check_winner(self):
         for x in self.board:
             if sum(x) == 3 or sum(x) == -3:
-                print("player " + str(self.turn) + " is the winner")
                 return self.turn
 
         scored1 = scored2 = 0
@@ -20,11 +22,9 @@ class Game:
             for y in range(3):
                 score += self.board[y][x]
             if score == 3 or score == -3:
-                print("player " + str(self.turn) + " is the winner")
                 return self.turn
 
         if scored1 == 3 or scored1 == -3 or scored2 == 3 or scored2 == -3:
-            print("player " + str(self.turn) + " is the winner")
             return self.turn
 
     def no_winner(self):
@@ -34,39 +34,90 @@ class Game:
                     return False
         return True
 
+    def game_finished(self):
+        return self.check_winner() or self.no_winner()
+
+    def place_turn(self, x, y):
+        if not self.board[x][y]:
+            self.board[x][y] = self.turn
+            self.turn *= -1
+            return True
+        else: return False
+
+    def get_available_moves(self):
+        available_moves = []
+
+        for x in range(len(self.board)):
+            for y in range(len(self.board[x])):
+                if not self.board[x][y]:
+                    available_moves.append([x, y])
+
+        return available_moves
+
 #### Machine learning stuff
-def score(game):
+choice = []
+
+def score(game, depth):
     if game.check_winner() is 1:
-        return 10
+        return 10 - depth
     elif game.check_winner() is -1:
-        return -10
+        return depth - 10
     else: return 0
 
-def minimax(game):
+def minimax(game, depth=0):
+    global choice
+
+    if game.game_finished(): return score(game, depth)
+
+    depth += 1
     scores = []
     moves = []
     
+    # Populate the scores array, recursing as needed
+    for move in game.get_available_moves():
+        possible_game = deepcopy(game)
+        possible_game.place_turn(move[0], move[1])
+
+        scores.append(minimax(possible_game, depth))
+        moves.append(move)
+
+    # Do the min or the max calculation
+    if game.turn is 1:
+        max_score_index = scores.index(max(scores))
+        choice = moves[max_score_index]
+        return scores[max_score_index]
+    else:
+        min_score_index = scores.index(min(scores))
+        choice = moves[min_score_index]
+        return scores[min_score_index]
 
 #### End of machine learning stuff
 
+# Create a new game
 game = Game()
 
-game.print_board()
-
 while True:
-    moveX = int(input())
-    moveY = int(input())
-    
-    if not game.board[moveX][moveY]:
-        # TODO move this to the game class
-        game.board[moveX][moveY] = game.turn
-        if game.check_winner():
-            break
-        if game.no_winner():
-            print("Tie")
-            break
-        game.turn *= -1
-    
-    game.print_board()
+    if game.game_finished():
+        if game.check_winner() is 1:
+            print("Player 1 won")
+        elif game.check_winner() is -1:
+            print("Player -1 won")
+        else: print("Tie")
+        break
+
+    if game.turn is 1:
+        game.print_board()
+
+        moveX = int(input("x: "))
+        moveY = int(input("y: "))
+        
+        if not game.place_turn(moveX, moveY):
+            print("Not a valid play, try again.")
+
+        game.print_board()
+    else:
+        # Let the machine play a turn
+        minimax(game)
+        game.place_turn(choice[0], choice[1])
 
 game.print_board()
