d8e9bc4e2f4135fc05078e16423cf715ede1c215
PhiliPdB <phlpdbrn@gmail.com>
2016-11-18 15:53:57 +0100

    Rewrite stuff

--- Code/ReinforcementLearning/main.py
+++ Code/ReinforcementLearning/main.py
@@ -4,23 +4,29 @@ from Player import Player
 from TicTacToe import TicTacToe
 
 
+def train(games):
+	# Set timer
+	startTime = clock()
+
+	# Train computer
+	for i in range(int(games)):
+		if i % 1000 == 0: print(i)
+		game = TicTacToe(p1, p2)
+		game.play_game()
+
+	deltaTime = clock() - startTime
+	m, s = divmod(deltaTime, 60)
+	print("It took " + str(int(m)) + " minutes and " + str(s) + " seconds to play " + str(games) + " games")
+
 # Assign the players
 p1 = QLearningPlayer()
 p2 = QLearningPlayer()
 
-# Set timer
-startTime = clock()
+# Do your own stuff
+train(int(1e6))
 
-# Train the computer
-for i in range(int(1e6)):
-	if i % 1000 == 0: print(i)
-	game = TicTacToe(p1, p2)
-	game.play_game()
 
-deltaTime = clock() - startTime
-m, s = divmod(deltaTime, 60)
-print("It took " + str(int(m)) + " minutes and " + str(s) + " seconds to play 1 million games")
-
+# Let the computer play againt a real player
 p1 = Player()
 p2.epsilon = 0
 
